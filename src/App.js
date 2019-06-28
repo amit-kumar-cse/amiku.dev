d4af4ed3a0370af829f3fb01452f9e0abad9df74
Amit Kumar <amitgarg.cse@gmail.com>
2019-06-28 17:35:42 +0530

    basic website.
    1. basic packages
    2. bootstrap mixins

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import bg from './images/bg.jpg';
+import './App.scss';
+
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/css/boostrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App container">
+            <div className='background-image-container'>
+                {/*<img src='http://sauravkumar.com/images/pexels-photo.jpg' alt={'laptop, notebook and coffee'}/>*/}
+                <img src={'./bg.jpg'} alt={'Background image'}/>
+            </div>
+            {/*<div className={'header'}></div>*/}
+            <section>
+                <div className='jumbotron text-center'>
+                    <h2 className='name'>Amit Kumar</h2>
+                    <h4 className='role'>Full Stack Software Professional</h4>
+                    <h5 className='role'>React | Java | AWS</h5>
+                    <div>
+                        <img src='./images/amit-kumar.jpg' alt={'Amit Kumar'}/>
+                    </div>
+                </div>
+
+            </section>
+            <footer>
+
+            </footer>
+        </div>
+    );
 }
 
 export default App;
