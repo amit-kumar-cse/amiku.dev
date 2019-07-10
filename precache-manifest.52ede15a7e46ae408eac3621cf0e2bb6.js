6c0361724c3ac0ef93e6da92f7a71c9992943a53
Amit Kumar <amitgarg.cse@gmail.com>
2019-07-10 16:35:21 +0530

    Updates

---
+++ precache-manifest.52ede15a7e46ae408eac3621cf0e2bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634c1bb3480c85021c7e63791b55a9b8",
+    "url": "/amitkumar.pro/index.html"
+  },
+  {
+    "revision": "df03f0775ae812918b87",
+    "url": "/amitkumar.pro/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bec6ee788289b5fa0dcf",
+    "url": "/amitkumar.pro/static/css/main.12878be6.chunk.css"
+  },
+  {
+    "revision": "df03f0775ae812918b87",
+    "url": "/amitkumar.pro/static/js/2.b477a215.chunk.js"
+  },
+  {
+    "revision": "bec6ee788289b5fa0dcf",
+    "url": "/amitkumar.pro/static/js/main.dab2ca5e.chunk.js"
+  },
+  {
+    "revision": "efe630159c527ebfa02b",
+    "url": "/amitkumar.pro/static/js/runtime~main.3cde013f.js"
+  }
+]);
