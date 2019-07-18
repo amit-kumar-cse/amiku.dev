1c139837e0827bed25777d728abaa94447fa6c3a
Amit Kumar <amitgarg.cse@gmail.com>
2019-07-18 14:27:26 +0530

    Enhanced about me.

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -9,15 +9,15 @@ const aboutMe = props => {
                 <h1 className="card-title text-primary">About</h1>
                 {/*<hr/>*/}
                 <div>
-                    <p>Software professional with 5.5 years of experience in full-stack system design and software
+                    <p>Software professional with close to 6 years of experience in system design and
                         development across Banking and Advertising domains at Amazon, Morgan Stanley and Adobe.</p>
 
-                    <ul>
+                    <ul className={commonClasses.listStyle}>
+                        <li>Proficient in React/Angular web development ecosystem.</li>
                         <li>Proficient in server-side technologies like Java-Spring Ecosystem, Object Oriented
                             Design.</li>
-                        <li>Proficient in Angular/React web development ecosystem.</li>
                         <li>Hands-on experience in AWS, Relational and NoSQL Databases.</li>
-                        <li>Expert at data structure and algorithms, clean system design.</li>
+                        <li>Expert at data structure and algorithms, clean and performant system design.</li>
                     </ul>
                 </div>
             </div>
