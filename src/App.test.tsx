a7522b01f90f3711f4e61a7c51605a6322781a97
Amit Kumar <amitgarg.cse@gmail.com>
2020-12-29 17:47:33 +0530

    My first tests with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { mount, shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-it("renders without crashing", () => {});
+it("renders without crashing", () => {
+  const shallowWrapper = shallow(<App />);
+  // const mountedWrapper = mount(<App />);
+  console.log(shallowWrapper.debug());
+  // console.log(mountedWrapper.debug());
+  expect(shallowWrapper).toBeTruthy();
+});
