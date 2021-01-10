08a03cd228f36bcc979a17250a78918b1ae83dfe
Amit Kumar <amitgarg.cse@gmail.com>
2021-01-10 21:52:36 +0530

    Linkedin link correction

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
   };
 
   let navigateToLinkedin = () => {
-    window.open("https://www.linkedin.com/in/amitkudev/");
+    window.open("https://www.linkedin.com/in/amikudev/");
   };
 
   let navigateToTwitter = () => {
